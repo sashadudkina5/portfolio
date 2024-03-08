9f2cac7b63803edd531f149fa54c65aeb4de8d4f
sashadudkina5 <uu.aaa@mail.ru>
2024-03-08 19:01:23 +0400

    fixed projects section styles

--- src/components/ProjectsList/ProjectsList.tsx
+++ src/components/ProjectsList/ProjectsList.tsx
@@ -5,11 +5,11 @@ import {projects} from "../../utils/projects"
 
 const ProjectsList = () => {
     return (
-        <section>
+        <section className={ProjectsListStyles.projects_section}>
             <h1 className={ProjectsListStyles.title}>Projects</h1>
       <ul className={ProjectsListStyles.project_list}>
         {projects.map((project) => (
-          <li key={project.title} className="project-item">
+          <li key={project.title} className={ProjectsListStyles.projects_item}>
             <ProjectCard 
               title={project.title}
               coverImageDesktop={project.coverImageDesktop}
